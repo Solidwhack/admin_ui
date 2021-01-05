f41934d2502859c13f466caf03da786d944c2d90
Solidwhack <75421671+Solidwhack@users.noreply.github.com>
2021-01-06 00:42:04 +0800

    Add files via upload

---
+++ lib/Restaurants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:admin_ui/navbar.dart';
+
+
+class Restaurants extends StatefulWidget {
+  @override
+  _RestaurantsState createState() => _RestaurantsState();
+}
+
+class _RestaurantsState extends State<Restaurants> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+
+
+        body: new Center(
+        child: new Text("hello"),
+    )
+    );
+  }
+
+}
